ca50ad5d3f5de9aafdafe670a958fd3b884e8a73
Ykhan183 <110489057+Ykhan183@users.noreply.github.com>
2022-08-03 19:11:36 +0530

    Add files via upload

---
+++ Que10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int DD,MM,YY;
+  printf("enter the date");
+  scanf("%d/%d/%d",&DD,&MM,&YY);
+
+  printf("date of %d-%d-%d",DD,MM,YY);
+  return 0;
+}
---
+++ Que11.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int HH,MM;
+    printf("enter the time");
+    scanf("%d%d",&HH,&MM);
+
+    printf("Time is %d Hour and %d minute",HH,MM);
+    return 0;
+}
